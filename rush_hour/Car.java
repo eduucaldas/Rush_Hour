f9d24bb608cd6498fe6b60328efa01ecd4424bc8
Eduardo Caldas <eduucaldas@gmail.com>
2018-01-18 14:53:49 +0100

    well tested

--- rush_hour/Car.java
+++ rush_hour/Car.java
@@ -2,26 +2,28 @@ package rush_hour;
 
 import java.util.StringTokenizer;
 
-public class Car {
+public class Car implements Comparable<Car>{
 	final static int SMALL = 2;
 	final static int LARGE = 3;
 	final static char VERTICAL = 'v';
 	final static char HORIZONTAL = 'h';
 	private final int id;
+	private final boolean red;
 	private final int len;
 	private final char dir;
-	private final boolean red;
 	public int x;
 	public int y;
 
-	public Car(int id, boolean red, int len, char dir, int x, int y) {
-		this.red = red;
+	public Car(int id, char dir, int len, int x, int y) throws IllegalArgumentException {
+		
 
 		if(id>0)
 			this.id = id;
 		else
 			throw new IllegalArgumentException("id = " + id + " Should be greater than 0");
-
+		
+		this.red = (id == 1);
+		
 		if(len == SMALL || len == LARGE)
 			this.len = len;
 		else
@@ -40,7 +42,8 @@ public class Car {
 			throw new IllegalArgumentException("x,y = " + dir + " Should be bigger than zero");
 		}
 
-		public Car(String line){
+	//ATTENTION: in input (x,y) go from 1 to len, but our att come from 0 to len-1
+	public Car(String line) throws IllegalArgumentException{
 		StringTokenizer st = new StringTokenizer(line);
 
 		int id = Integer.parseInt(st.nextToken());
@@ -75,8 +78,7 @@ public class Car {
 	}
 
 	public int get_type(){
-
-		return (this.len - 1) + ((dir == HORIZONTAL)? 0 : 2);
+		return (this.len() - 1) + ((this.dir() == HORIZONTAL)? 0 : 2);
 	}
 
 	public int id() {
@@ -87,20 +89,96 @@ public class Car {
 		return this.len;
 	}
 
-	public int dir() {
+	public char dir() {
 		return this.dir;
 	}
 
 	public boolean red() {
 		return this.red;
 	}
-
-
-
-	public static void main(){
-		String line = "1 h 2 2 3";
-		Car dolly = new Car(line);
+	
+	@Override
+	public String toString() {
+		return "Car(id = " + this.id() + ", red = " + this.red() + ", len = " + this.len() + ", dir = " + this.dir() + ", (x, y) = (" + this.x + ", " + this.y + "))";
 	}
+	
+	@Override
+	public int compareTo(Car other) {
+		//IMPORTANT: This method compares two cars in the following order:
+		// 1. types
+		// 2. position, the most important position being the one orthogonal to the car direction.
+		int comp_type = this.get_type() - other.get_type();
+		if(comp_type == 0) {
+			int comp_1 = 0;
+			int comp_2 = 0;
+			if(this.dir() == VERTICAL) {
+				comp_1 = this.x - other.x;
+				comp_2 = this.y - other.y;
+			}
+			else {
+				comp_1 = this.y - other.y;
+				comp_2 = this.x - other.x;
+			}
+			
+			if(comp_1 == 0)
+				return comp_2;
+			else
+				return comp_1;
+		}
+		else return comp_type;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		return this.compareTo((Car)other) == 0;
+	}
+	
+	public static boolean collision(Car one, Car other) {
+		if(one.dir() != other.dir()) {
+			Car h, v;
+			
+			if(one.dir() == HORIZONTAL) {
+				h = one;
+				v = other;
+			}
+			else {
+				h = other;
+				v = one;
+			}
+			
+			//draw it and see it. the axis are counted from top left
+			return (h.y - v.y >= 0) && (h.y - v.y <= v.len - 1) && (v.x - h.x >= 0) && (v.x - h.x <= h.len - 1);
+		}
+		else {
+			if(one.dir() == VERTICAL && one.x == other.x) {
+				Car up, down;
+				if(one.y <= other.y) {
+					up = one;
+					down = other;
+				}
+				else {
+					up = other;
+					down = one;
+				}
+				return (down.y - up.y) <= up.len - 1;
+			}
+			else if(one.dir() == HORIZONTAL  && one.y == other.y) {
+				Car left, right;
+				if(one.y <= other.y) {
+					left = one;
+					right = other;
+				}
+				else {
+					left = other;
+					right = one;
+				}
+				return (left.x - right.x) <= left.len - 1;
+			}
+		}
+		return false;
+			
+	}
+	
 }
 
 

e9d7bb52ba788d52d4a885f91603212941212be2
Eduardo Caldas <eduucaldas@gmail.com>
2018-01-18 01:12:47 +0100

    tabs

--- rush_hour/Car.java
+++ rush_hour/Car.java
@@ -16,58 +16,57 @@ public class Car {
 	public Car(boolean red, int len, char dir, int x, int y) {
 		this.red = red;
 
-        if(len == SMALL || len == LARGE)
-        	this.len = len;
-        else
-        	throw new IllegalArgumentException("len = " + len + " Should be 2 or 3");
+		if(len == SMALL || len == LARGE)
+			this.len = len;
+		else
+			throw new IllegalArgumentException("len = " + len + " Should be 2 or 3");
 
-        if(dir == VERTICAL || dir == HORIZONTAL)
-        	this.dir = dir;
-        else
-        	throw new IllegalArgumentException("dir = " + dir + " Should be v or h");
+		if(dir == VERTICAL || dir == HORIZONTAL)
+			this.dir = dir;
+		else
+			throw new IllegalArgumentException("dir = " + dir + " Should be v or h");
 
-        if(x >= 0 && y >= 0){
-        	this.x = x;
-        	this.y = y;
-        }
-        else
-        	throw new IllegalArgumentException("x,y = " + dir + " Should be bigger than zero");
-	}
+		if(x >= 0 && y >= 0){
+			this.x = x;
+			this.y = y;
+		}
+		else
+			throw new IllegalArgumentException("x,y = " + dir + " Should be bigger than zero");
+		}
 
-	public Car(String line){
+		public Car(String line){
 		StringTokenizer st = new StringTokenizer(line);
 
-        int id = Integer.parseInt(st.nextToken());
-        char dir = st.nextToken().toString().charAt(0);
-        int len = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
+		int id = Integer.parseInt(st.nextToken());
+		char dir = st.nextToken().toString().charAt(0);
+		int len = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
 
-        this.red = (id == 1);
+		this.red = (id == 1);
 
-        if(len == SMALL || len == LARGE)
-        	this.len = len;
-        else
-        	throw new IllegalArgumentException("len = " + len + " Should be 2 or 3");
+		if(len == SMALL || len == LARGE)
+			this.len = len;
+		else
+			throw new IllegalArgumentException("len = " + len + " Should be 2 or 3");
 
-        if(dir == VERTICAL || dir == HORIZONTAL)
-        	this.dir = dir;
-        else
-        	throw new IllegalArgumentException("dir = " + dir + " Should be v or h");
+		if(dir == VERTICAL || dir == HORIZONTAL)
+			this.dir = dir;
+		else
+			throw new IllegalArgumentException("dir = " + dir + " Should be v or h");
 
-        if(x >= 0 && y >= 0){
-        	this.x = x;
-        	this.y = y;
-        }
-        else
-        	throw new IllegalArgumentException("x,y = " + dir + " Should be bigger than zero");
+		if(x >= 0 && y >= 0){
+			this.x = x;
+			this.y = y;
+		}
+		else
+			throw new IllegalArgumentException("x,y = " + dir + " Should be bigger than zero");
 	}
 
 	public int get_type(){
 
 		return (this.len - 2) + ((dir == HORIZONTAL)? 1 : 0);
 	}
-
 
 	public int len() {
 		return this.len;
@@ -80,6 +79,8 @@ public class Car {
 	public boolean red() {
 		return this.red;
 	}
+
+
 
 	public static void main(){
 		String line = "1 h 2 2 3";

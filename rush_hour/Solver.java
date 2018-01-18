53d09dda2cd66ad30ce540d7799da1ba46442886
Ian <i.duleba@gmail.com>
2018-01-18 01:29:26 +0100

    1st day

--- rush_hour/Solver.java
+++ rush_hour/Solver.java
@@ -1,5 +1,49 @@
 package rush_hour;
 
+import java.io.*;
+import java.util.*;
+
 public class Solver {
 
+    public void read_file(String fileName){
+        //we will assume that the file has the correct format
+        String line = null;
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            State initial_state = new State();
+
+            //first line is the size of the board
+            initial_state.set_size(Integer.parseInt(bufferedReader.readLine()));
+
+            //second line is the number of cars
+            initial_state.set_number_of_cars(Integer.parseInt(bufferedReader.readLine()));
+
+            StringTokenizer st;
+
+            while((line = bufferedReader.readLine()) != null) {//read line
+
+                Car car = new Car(line);
+                
+                initial_state.add_car(car);
+
+                initial_state.print_state();
+
+                }
+
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+    }
+
+    public static void main(String [] args) {
+        Solver game = new Solver();
+        game.read_file("test");
+    }
 }
--- rush_hour/State.java
+++ rush_hour/State.java
@@ -1,5 +1,68 @@
 package rush_hour;
 
+import java.io.*;
+import java.util.*;
+
 public class State {
 
+	private int size;
+    private int number_of_cars;
+
+	private ArrayList<Car> type_1;//horizontal with length 2
+    private ArrayList<Car> type_2;//horizontal with length 3
+    private ArrayList<Car> type_3;//vertical with length 2
+    private ArrayList<Car> type_4;//vertical with length 3
+
+    public State(){
+        this.size = 0;
+        this.number_of_cars = 0;
+        type_1 = new ArrayList<Car>();
+        type_2 = new ArrayList<Car>();
+        type_3 = new ArrayList<Car>();
+        type_4 = new ArrayList<Car>();
+    }
+
+    public void set_size(int size){
+    	this.size = size;
+    }
+
+    public int get_size(){
+    	return this.size;
+    }
+
+    public void set_number_of_cars(int number_of_cars){
+    	this.number_of_cars = number_of_cars;
+    }
+
+    public int get_number_of_cars(){
+    	return this.number_of_cars;
+    }
+
+    public boolean is_valid(Car car){
+    	//TODO
+    	return true;
+    }
+
+    public void print_state(){
+    	System.out.println("TODO");
+    }
+
+    public boolean add_car(Car car){
+    	if(!is_valid(car)) return false;
+    	switch(car.get_type()){
+            case 1: 
+                type_1.add(car);
+                break;
+            case 2:
+                type_2.add(car);
+                break;
+            case 3:
+                type_2.add(car);
+                break;
+            case 4:
+                type_2.add(car);
+                break;
+    	}
+    	return true;
+    }
 }

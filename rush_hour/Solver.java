a6160e1393916e939e7a7df0f103e60ebf7c3de7
Eduardo Caldas <eduucaldas@gmail.com>
2018-01-18 20:50:07 +0100

    backbone

--- rush_hour/Solver.java
+++ rush_hour/Solver.java
@@ -4,6 +4,31 @@ import java.io.*;
 import java.util.*;
 
 public class Solver {
+    //which data structure should we use for the explored states
+    Set<State> explored;
+    Queue<State> border;
+
+    public Solver(String fileName) {
+        this.explored = new TreeSet<State>();
+        this.border = new LinkedList<State>();
+        this.border.add(Solver.file_to_state(fileName));
+    }
+
+    public void evolution() {
+
+    }
+
+    //need a function possible states
+    //will just put all valid states to border
+    private LinkedState possible_moves(State actual) {
+
+    }
+
+    //Need a function file_to_state
+    private State file_to_state(String fileName) {
+
+    }
+
     public void read_file(String fileName){
         //we will assume that the file has the correct format
         String line = null;
@@ -27,12 +52,9 @@ public class Solver {
 
                 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 //tenho que fazer um if aqui pra ver se adicionou corretamente
-                if(!initial_state.add_car(car)){
-                	System.out.println("Car Overlapping!");
-                	return;
-                }
+                initial_state.add_car(car);
 
-            }
+                }
 
             initial_state.print_state();
 

6baff63b5f6432d0c26b9ed5e861135b1a17eedf
Eduardo Caldas <eduucaldas@gmail.com>
2018-01-31 10:04:33 +0100

    UI

--- src/rush_hour/Tester.java
+++ src/rush_hour/Tester.java
@@ -1,6 +1,9 @@
 package rush_hour;
 
 import java.util.Comparator;
+import java.util.Scanner;
+
+import junit.framework.Test;
 
 public class Tester {
 	String[] files;
@@ -37,25 +40,83 @@ public class Tester {
 		}
 	}
 	
+	public static String pickInput(Scanner sc) {
+		System.out.println("Great! Please enter the name of your test file, it should be in the tests folder, and don't bother writing .txt");
+		System.out.print("filename: ");
+		String filename = sc.nextLine();
+		return "tests/" + filename + ".txt";
+	}
+	
+	public static Comparator<State> cmpSelector(Scanner sc){
+		System.out.println("Choose your heuristics");
+		System.out.println("1. Zero Heuristics");
+		System.out.println("2. Blocking Heuristics");
+		System.out.println("3. My Heuristics");
+		System.out.println("4. Non Consistant Heuristics");
+		String answer = "";
+		while(!answer.equals("1") && !answer.equals("2") && !answer.equals("3") && !answer.equals("4")) {
+			System.out.print("Answer[1, 2, 3, 4]: ");
+			answer = sc.nextLine();
+		}
+		Comparator <State> cmp = null;
+		switch (answer) {
+			case "1": 
+				cmp = new Zero_Comparator();
+				break;
+			case "2":
+				cmp = new Blocking_Comparator();
+				break;
+			case "3":
+				cmp = new My_Comparator();
+				break;
+			case "4":
+				cmp = new Non_Consistant_Comparator();
+				break;
+		}
+		return cmp;
+	}
+	
 	public static void main(String[] args) throws Exception {
-		String fileTest = "tests/RushHour1.txt"; // Single test file, you should probably change here
-		boolean see_intermediary_solutions = false; // do you want?
-		//uncomment lines below to use other heuristics
-		Comparator<State> cmp;
-		cmp = new My_Comparator();
-		// cmp = new Blocking_Comparator();
-		// cmp = new Non_Consistant_Comparator();
-		// cmp = new Zero_Comparator();
+		System.out.println("Welcome to the rush hour solver!");
+		String filename, answer = "";
+		Scanner sc = new Scanner(System.in);
+		while(!answer.equals("y")  && !answer.equals("n")) {
+			System.out.println("Do you want to see it solve your rush hour puzzle?[y/n]");
+			answer = sc.nextLine().toLowerCase();
+			
+		}
+		if(answer.equals("y")) {
+			SolverTest(pickInput(sc), true, new My_Comparator());
+		}
+		System.out.println("Maybe you`re more of a nerd. What do you want to know?");
+		System.out.println("1. What is the bitmap you told about?");
+		System.out.println("2. Could I use your Solver with different heuristics?");
+		System.out.println("3. Could I compare your Solver with different heuristics and do loads of tests at the same time? :)");
+		System.out.println("4. Who are you calling a nerd?");
 		
-		Tester.StateTest(fileTest);
-		
-		Tester.SolverTest(fileTest, see_intermediary_solutions, new My_Comparator());
-		//If it's too slow for you, go to Solver and change time_sleep_print
-		
-		String base = "tests/test"; //Change here to fit you test files
-		int number_of_tests = 40; //also here
-		String extension = ".txt"; // probably not here
-		Tester test = new Tester(number_of_tests, base, extension);
-		test.CompareTest();
+		while(!answer.equals("1") && !answer.equals("2") && !answer.equals("3") && !answer.equals("4")) {
+			System.out.print("Answer[1, 2, 3, 4]: ");
+			answer = sc.nextLine();
+		}
+		switch (answer) {
+		case "1": 
+			StateTest(pickInput(sc));
+			System.out.println();
+			System.out.println("See? The bit map is like a shadow!");
+			break;
+		case "2":
+			SolverTest(pickInput(sc), true, cmpSelector(sc));
+			break;
+		case "3":
+			System.out.println("Pleas put the tests in a tests folder and set their name to a format testXX, with XX digits");
+			System.out.print("How many tests do you have?");
+			Tester test = new Tester(sc.nextInt(), "tests/test", ".txt");
+			test.CompareTest();
+			break;
+		case "4":
+			System.out.println("Ok ok, no one here is a nerd... Chill...");
+			break;
+		}
+
 	}
 }
